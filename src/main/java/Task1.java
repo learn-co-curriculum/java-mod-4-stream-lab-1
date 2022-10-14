99d942ec19d427337524fa5beea08d1d3527c98b
Linda Seiter <linseiter@gmail.com>
2022-10-14 17:46:26 -0400

    adapt lab with 3 tasks involving filter, count, min, max, foreach methods (#1)

---
+++ src/main/java/Task1.java
@@ -0,0 +1,13 @@
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        double[] pricePerGallon = {3.50, 3.25, 4.64, 3.95};
+
+        //Call the stream count method and print the number of values in the array
+
+        //Call the stream max method to get the maximum price
+        //Print the highest price per gallon (if a maximum value exists)
+
+    }
+}
